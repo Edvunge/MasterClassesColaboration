fa79a6cc09edd35ac4ece2099df04964b228c897
Daniel Kololo <danielkololo2018@gmail.com>
2024-03-16 23:38:19 +0100

    exerc01_update_teclado

--- exercicios/Parte_1_exercsVariaveis/Exerc01.java
+++ exercicios/Parte_1_exercsVariaveis/Exerc01.java
@@ -14,19 +14,9 @@ public class Exerc01 {
         Scanner sc = new Scanner(System.in);
         //int d;// não esta errado mais recomenda-se que se use nome de variaveis mais intuitivos
         int number;
-
         System.out.println("Digite um numero inteiro: ");
-<<<<<<< HEAD
-        d= sc.nextInt();
-        System.out.println("Numero inteiro: "  + d);
-        sc.close(); // sc.close serve para fechar o programa ou  objeto sc.
-=======
         number = sc.nextInt();
-
         System.out.println("Numero inteiro: "  + number);
-
-
-        sc.close(); // descubrit para que serve o close()
->>>>>>> 43ab01eecfb8159eeeedb68c6b60a5fe792c3155
+        sc.close(); // O close() serve para fechar o programa ou  objeto sc
     }
 }
