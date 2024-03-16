d49c4435d2ecec187573f7f3616a1dbb0dbf73b2
edvunge <costaedy18@gmail.com>
2024-03-16 16:41:57 +0000

    variaveis_and_exercs

---
+++ exercicios/Parte_1_exercsVariaveis/Exerc01.java
@@ -0,0 +1,13 @@
+package exercicios.Parte_1_exercsVariaveis;
+
+public class Exerc01 {
+    public static void main(String[] args) {
+        // 1. Faca um programa que leia um numero inteiro e o imprima.
+        int numero = 2;
+
+        System.out.println("numero inteiro: " + numero);
+
+        // como receber comndos pelo teclado, neste exercio
+        // regra commit: "exerc01_update_teclado"
+    }
+}
---
+++ javaBasico/Variables.java
@@ -0,0 +1,27 @@
+package javaBasico;
+
+public class Variables {
+    public static void main(String[] args) {
+        // char, int, double, float, bool , (long , short)
+
+        // inteiro
+        int age = 20; // -2 -1 0 1 2
+        // int peso = (int) 78.9; // conversao explicita
+
+        // double / float
+        double peso = 78.9;
+        float altura = 1.90f;
+
+        // char
+        char letra = 'e';
+
+        // boolean
+        boolean vcEhAlto = true; // 1 ou 0 verdadeiro ou falso
+
+        System.out.println("Peso: "+ peso);
+        System.out.println("idade: " + age);
+        System.out.println("altura: " + altura);
+        System.out.println("letra: " + letra);
+        System.out.println("voce eh alto: " + vcEhAlto);
+    }
+}
