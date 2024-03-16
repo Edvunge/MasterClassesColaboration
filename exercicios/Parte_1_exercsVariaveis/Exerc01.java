8ede0ed6f5fded692a28d47cc4e6d85a5824b70a
Daniel Kololo <danielkololo2018@gmail.com>
2024-03-16 19:53:33 +0100

    exerc01_update_teclado

--- exercicios/Parte_1_exercsVariaveis/Exerc01.java
+++ exercicios/Parte_1_exercsVariaveis/Exerc01.java
@@ -1,5 +1,7 @@
 package exercicios.Parte_1_exercsVariaveis;
 
+import java.util.Scanner;
+
 public class Exerc01 {
     public static void main(String[] args) {
         // 1. Faca um programa que leia um numero inteiro e o imprima.
@@ -9,5 +11,11 @@ public class Exerc01 {
 
         // como receber comndos pelo teclado, neste exercio
         // regra commit: "exerc01_update_teclado"
+         Scanner sc = new Scanner(System.in);
+        int d;
+        System.out.println("Digite um numero inteiro: ");
+        d= sc.nextInt();
+        System.out.println("Numero inteiro: "  + d);
+        sc.nextInt();10
     }
 }
