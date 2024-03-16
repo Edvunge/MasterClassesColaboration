63572520093b42d974df4088f816e74d5fcf705b
Daniel Kololo <danielkololo2018@gmail.com>
2024-03-16 17:04:20 +0100

    DanielUpdate

--- javaBasico/HelloWorldMain.java
+++ javaBasico/HelloWorldMain.java
@@ -3,7 +3,9 @@ package javaBasico;
 public class HelloWorldMain {
     public static void main(String[] args) {
         String nome1 = "Edvaldo";
+        String nome2 = "Daniel";
         System.out.println("ola teste geral.");
         System.out.println("autor1: " + nome1);
+        System.out.println("autor2: " + nome2);
     }
 }
