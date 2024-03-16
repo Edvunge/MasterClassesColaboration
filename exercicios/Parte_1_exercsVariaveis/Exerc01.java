43ab01eecfb8159eeeedb68c6b60a5fe792c3155
edvunge <costaedy18@gmail.com>
2024-03-16 20:12:14 +0000

    exerc01_update_fix_and_clean

--- exercicios/Parte_1_exercsVariaveis/Exerc01.java
+++ exercicios/Parte_1_exercsVariaveis/Exerc01.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
 public class Exerc01 {
     public static void main(String[] args) {
         // 1. Faca um programa que leia um numero inteiro e o imprima.
-        int numero = 2;
 
-        System.out.println("numero inteiro: " + numero);
+        // Retificaçao do exercicio: 16/03/2024 edvunge 20:10min
 
         // como receber comndos pelo teclado, neste exercio
         // regra commit: "exerc01_update_teclado"
-         Scanner sc = new Scanner(System.in);
-        int d;
+
+        Scanner sc = new Scanner(System.in);
+        //int d;// não esta errado mais recomenda-se que se use nome de variaveis mais intuitivos
+        int number;
+
         System.out.println("Digite um numero inteiro: ");
-        d= sc.nextInt();
-        System.out.println("Numero inteiro: "  + d);
-        sc.nextInt();
+        number = sc.nextInt();
+
+        System.out.println("Numero inteiro: "  + number);
+
+
+        sc.close(); // descubrit para que serve o close()
     }
 }
