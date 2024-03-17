c3152c00f31a933f18c5cc89d93e0e40e4c5de58
edvunge <costaedy18@gmail.com>
2024-03-17 17:43:55 +0000

    exercs02_fix_and_clean

--- exercicios/Parte_1_exercsVariaveis/Exerc01.java
+++ exercicios/Parte_1_exercsVariaveis/Exerc01.java
@@ -5,20 +5,18 @@ import java.util.Scanner;
 public class Exerc01 {
     public static void main(String[] args) {
         // 1. Faca um programa que leia um numero inteiro e o imprima.
-
         // Retificaçao do exercicio: 16/03/2024 edvunge 20:10min
-
         // como receber comndos pelo teclado, neste exercio
         // regra commit: "exerc01_update_teclado"
 
         Scanner sc = new Scanner(System.in);
-        //int d;// não esta errado mais recomenda-se que se use nome de variaveis mais intuitivos
         int number;
+
         System.out.println("Digite um numero inteiro: ");
         number = sc.nextInt();
+
         System.out.println("Numero inteiro: "  + number);
-        sc.close(); // O close() serve para fechar o programa ou  objeto sc.
+
+        sc.close();
     }
-
-
 }
--- exercicios/Parte_1_exercsVariaveis/Exerc02.java
+++ exercicios/Parte_1_exercsVariaveis/Exerc02.java
@@ -1,16 +1,19 @@
 package exercicios.Parte_1_exercsVariaveis;
 
-
 import java.util.Scanner;
 
 public class Exerc02 {
     public static void main(String[] args) {
-        //2. Fac¸a um programa que leia um n´umero real e o imprima.
-        Scanner sc = new Scanner(System.in); // Scanner para ler o numero digitado.
-        double realnumber;//Tipo de variavel.
-        System.out.println("Digite um numero real: ");//solicitar digitacao do numero.
-        realnumber = sc.nextDouble();//ler o numero real digitado.
-        System.out.println("O numero digitado é : "  + realnumber);//imprimir o numero real.
-        sc.close();//fecha o Scanner.
+        //2. Faca um programa que leia um numero real e o imprima.
+
+        Scanner sc = new Scanner(System.in);
+        double realNumber;
+
+        System.out.println("Digite um numero real: ");
+        realNumber = sc.nextDouble();
+
+        System.out.println("O numero digitado eh : "  + realNumber);
+
+        sc.close();
     }
 }
